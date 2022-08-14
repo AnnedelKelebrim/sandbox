e5470ed9e163e92688041f02062e62bcea19498c
AnnedelKelebrim <annedel@mail.ru>
2022-08-14 22:52:17 +0600

    4 пункт задания

--- src/main/java/ru/stqa/pft/sandbox/DistanceCalc.java
+++ src/main/java/ru/stqa/pft/sandbox/DistanceCalc.java
@@ -4,14 +4,10 @@ public class DistanceCalc {
     public static void main(String[] args) {
         Point a = new Point(2, 7);
         Point b = new Point(5.8, 12.1);
-        double d = distance(a, b);
+        double d = Point.distance(a, b);
         System.out.println("Рассточние от точки а (x = " + a.x + ", y = " + a.y + ") до точки b (x = " + b.x + ", y = " + b.y + ") = " + d);
     }
 
-    public static double distance(Point p1, Point p2) {
-        double diff1 = p2.x - p1.x;
-        double diff2 = p2.y - p1.y;
-        return Math.sqrt((diff1 * diff1) + (diff2 * diff2));
-    }
+
 }
 
--- src/main/java/ru/stqa/pft/sandbox/Point.java
+++ src/main/java/ru/stqa/pft/sandbox/Point.java
@@ -8,4 +8,9 @@ public class Point {
         this.x = x;
         this.y = y;
     }
+    public static double distance(Point p1, Point p2) {
+        double diff1 = p2.x - p1.x;
+        double diff2 = p2.y - p1.y;
+        return Math.sqrt((diff1 * diff1) + (diff2 * diff2));
+    }
 }
