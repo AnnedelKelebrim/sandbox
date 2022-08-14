f81afb39f20199b3e6f158a18ca73066119afb30
AnnedelKelebrim <annedel@mail.ru>
2022-08-15 00:02:21 +0600

    Новый вариант решения и убрала забытый статик

--- src/main/java/ru/stqa/pft/sandbox/DistanceCalc.java
+++ src/main/java/ru/stqa/pft/sandbox/DistanceCalc.java
@@ -4,10 +4,8 @@ public class DistanceCalc {
     public static void main(String[] args) {
         Point a = new Point(2, 7);
         Point b = new Point(5.8, 12.1);
-        double d = Point.distance(a, b);
+        double d = a.distance(b);
         System.out.println("Расстояние от точки а (x = " + a.x + ", y = " + a.y + ") до точки b (x = " + b.x + ", y = " + b.y + ") = " + d);
     }
-
-
 }
 
--- src/main/java/ru/stqa/pft/sandbox/Point.java
+++ src/main/java/ru/stqa/pft/sandbox/Point.java
@@ -8,9 +8,9 @@ public class Point {
         this.x = x;
         this.y = y;
     }
-    public static double distance(Point p1, Point p2) {
-        double diff1 = p2.x - p1.x;
-        double diff2 = p2.y - p1.y;
+    public double distance(Point p) {
+        double diff1 = p.x - this.x;
+        double diff2 = p.y - this.y;
         return Math.sqrt((diff1 * diff1) + (diff2 * diff2));
     }
 }
