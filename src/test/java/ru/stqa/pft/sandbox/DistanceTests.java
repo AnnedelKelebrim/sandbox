0dbb34b18452b978a362457b537e2e4c4e963a04
AnnedelKelebrim <annedel@mail.ru>
2022-08-18 18:42:45 +0600

    Задание №3, исправленное.

--- src/test/java/ru/stqa/pft/sandbox/DistanceTests.java
+++ src/test/java/ru/stqa/pft/sandbox/DistanceTests.java
@@ -8,20 +8,20 @@ public class DistanceTests {
     public void testDistance() {
         Point a = new Point(3, 7);
         Point b = new Point(9, 5);
-        Assert.assertEquals((int) a.distance(b), 6);
+        Assert.assertEquals(a.distance(b), 6.324555320336759);
     }
 
     @Test
     public void testDistanceNegativeNumbers() {
         Point a = new Point(-3, 7);
         Point b = new Point(9, -5);
-        Assert.assertEquals((int) a.distance(b), 16);
+        Assert.assertEquals(a.distance(b), 16.97056274847714);
     }
 
     @Test
     public void testDistanceZero() {
         Point a = new Point(0, 0);
         Point b = new Point(0, 0);
-        Assert.assertEquals((int) a.distance(b), 0);
+        Assert.assertEquals(a.distance(b), 0);
     }
 }
